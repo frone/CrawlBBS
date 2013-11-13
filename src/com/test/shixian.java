04e797fed52f5c3704c8982ec6a7e3399d1cec3c
frone <qiujing_xie@163.com>
2013-11-13 17:25:22 +0800

    adjust the structure
    adjust the structure

--- src/com/test/shixian.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-public class shixian {
-	    public static void main(String args[]){
-	    	Equals number=new Equals();
-	    	System.out.println("两个类变量比较:"+number.equals("I hate you."));
-	    }
-	}
